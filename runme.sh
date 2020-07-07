b38b09eb44511a0c999cd5a32eff2ad672d510ea
ae <ae>
2020-07-07 11:06:11 -0700

    adding plex install

--- runme.sh
+++ runme.sh
@@ -2,6 +2,20 @@
 
 SRC_DIR=~/src
 
+# Start GCP VM instance
+
+
+# Install Plex
+#  From: https://www.linode.com/docs/applications/media-servers/install-plex-media-server-on-ubuntu-18-04/
+
+wget https://downloads.plex.tv/plex-media-server-new/1.19.4.2935-79e214ead/debian/plexmediaserver_1.19.4.2935-79e214ead_amd64.deb
+sudo dpkg -i plexmediaserver*.deb
+sudo systemctl enable plexmediaserver.service
+sudo systemctl start plexmediaserver.service
+
+# Install VPN
+
+
 # NO IP
 
 #get no-ip updater
